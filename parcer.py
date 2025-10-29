5c01cf645069ac2bb299fdfdb425963590db8d5b
breeuz1 <dareter096gmail.com>
2025-10-29 03:38:16 +0300

    parser v2

--- parcer.py
+++ parcer.py
@@ -1,21 +1,38 @@
 import requests
+from bs4 import BeautifulSoup
+import json
 
+url = "https://habr.com/ru/articles"
 
-url = "https://newsapi.org/v2/top-headlines"
-category = input(
-    "Choose one category and write here(Categories: business; entertainment; general; health; science; sports; technology): "
-)
-response = requests.get(f"{url}?category={category}&apiKey=api_key")
-json_response = response.json()
-count_news = 1
-for news in json_response["articles"]:
-    print(
-        f"Articles {count_news}",
-        news["author"],
-        news["title"],
-        news["description"],
-        news["publishedAt"],
-        sep="\n",
+response = requests.get(url)
+
+allNews = []
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+articles = soup.find_all("article")
+
+new_list = []
+for article in articles:
+    title = article.find("h2").text.strip()
+    link = article.find("a")["href"]
+    author = article.find("a")["title"]
+    date = article.find("time")["title"]
+
+    if link.startswith("/"):
+        link = "https://habr.com" + link
+
+    new_list.append(
+        {
+            "title": title,
+            "url": link,
+            "author": author,
+            "date": date,
+        }
     )
-    count_news += 1
-    print()
+
+
+with open("news.json", "w", encoding="utf-8") as f:
+    json.dump(new_list, f, ensure_ascii=False, indent=2)
+
+print(f"Спаршено {len(new_list)} новостей!")
