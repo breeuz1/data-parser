06bf8e7c5c120564a5baa4ed962e3f8ba27101a7
breeuz1 <dareter096gmail.com>
2025-11-03 21:04:40 +0300

    parser v7

---
+++ news_habr_bot.py
@@ -0,0 +1,34 @@
+import telebot
+import os
+
+import json
+
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+bot = telebot.TeleBot(config["telegram_token"])
+
+
+@bot.message_handler(commands=["start"])
+def handle_start(message):
+    bot.send_message(
+        message.chat.id,
+        "Привет, это новостной бот Habr. Напиши что тебе нужно. Можешь воспользоваться командой /help",
+    )
+
+
+@bot.message_handler(commands=["help"])
+def handle_help(message):
+    bot.send_message(
+        message.chat.id, "Привет, это актуавльный список команд:\n - /news"
+    )
+
+
+@bot.message_handler(content_types=["text"])
+def echo(message):
+    bot.send_message(message.chat.id, f"Вы отпраили мне текст: '{message.text}'")
+
+
+print("Бот запущен!!!")
+
+bot.polling(non_stop=True)
