63fa1d24634a85411f61ee7a5078cc2bdad94bfe
breeuz1 <dareter096gmail.com>
2025-11-21 02:26:48 +0300

    parser v8

--- news_habr_bot.py
+++ news_habr_bot.py
@@ -1,7 +1,7 @@
 import telebot
-
-
 import json
+
+user_data = {}
 
 with open("data-parser/config.json", "r") as f:
     config = json.load(f)
@@ -20,13 +20,53 @@ def handle_start(message):
 @bot.message_handler(commands=["help"])
 def handle_help(message):
     bot.send_message(
-        message.chat.id, "Привет, это актуавльный список команд:\n - /news"
+        message.chat.id, "Привет, это актуавльный список команд:\n - /survey \n - /news"
     )
 
 
 @bot.message_handler(content_types=["text"])
 def echo(message):
+    if message.text.startswith('/'):
+        return
     bot.send_message(message.chat.id, f"Вы отпраили мне текст: '{message.text}'")
+
+
+@bot.message_handler(commands=["survey"])
+def start_survey(message):
+    user_id = message.from_user.id
+
+    if user_id not in user_data:
+        user_data[user_id] = {}
+
+    msg = bot.send_message(message.chat.id, "Как тебя зовут?")
+    bot.register_next_step_handler(msg, process_name_step)
+
+
+def process_name_step(message):
+    user_id = message.from_user.id
+    user_data[user_id]["name"] = message.text
+
+    msg = bot.send_message(message.chat.id, f"Приятно познакомиться! Сколько тебе лет?")
+    bot.register_next_step_handler(msg, process_age_step)
+
+
+def process_age_step(message):
+    user_id = message.from_user.id
+
+    try:
+        age = int(message.text)
+        user_data[user_id]["age"] = age
+
+        user_info = user_data[user_id]
+
+        bot.send_message(
+            message.chat.id,
+            f"Супер! Тебя зовут {user_info.get('name')}, тебе {user_info.get('age')} лет.",
+        )
+    except ValueError:
+        bot.send_message(
+            message.chat.id, "Возраст должен быть числом! Начни заново: /survey"
+        )
 
 
 print("Бот запущен!!!")
